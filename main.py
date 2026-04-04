372e845d3f97de46ef25d957b966127a29ea1fa0
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-04-04 22:46:34 +0600

    path function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -23,3 +23,13 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    #load all patient data
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'Patient not found!'}
+
