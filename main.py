f8a3ca77ce60372cae7d5662f36a320454ae49a9
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-04-04 23:18:23 +0600

    raising httpexception status code error with details

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -31,5 +31,5 @@ def view_patient(patient_id: str = Path(..., description='ID of the patient in t
 
     if patient_id in data:
         return data[patient_id]
-    return {'error':'Patient not found!'}
+    raise HTTPException(status_code=404, detail='Patient not found!')
 
