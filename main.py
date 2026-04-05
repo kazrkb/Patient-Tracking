30a55bc537be8594a59a45a9850641b47445a6df
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-04-05 15:22:06 +0600

    used query parameter for sorting with the order

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -33,3 +33,17 @@ def view_patient(patient_id: str = Path(..., description='ID of the patient in t
         return data[patient_id]
     raise HTTPException(status_code=404, detail='Patient not found!')
 
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight, bmi'), order: str = Query('asc', description='Sort in asc or dsc order')):
+    valid_fileds = ['height','weight','bmi']
+    if sort_by not in valid_fileds:
+        raise HTTPException(status_code=400, detail=f'invalid field select from {valid_fileds}')
+    order_by = ['asc', 'desc']
+    if order not in order_by:
+        raise HTTPException(status_code=400, detail=f'invalid order select between asc and dsc')
+    
+    data = load_data()
+    sort_order = True if order=='desc' else False
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
